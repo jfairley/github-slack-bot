ef487d968fd0ebea058dcca4fe67fbfb8080ea5d
Jeff Fairley <jfairley@gmail.com>
2019-01-30 00:59:17 -0700

    test: add missing env

--- test/initEnv.ts
+++ test/initEnv.ts
@@ -2,5 +2,6 @@ process.env = {
   ...process.env,
   GITHUB_TOKEN: 'test-github-token',
   GITHUB_WEBHOOK_PORT: '12345',
+  GITHUB_WEBHOOK_SECRET: 'test-github-webhook-secret',
   SLACK_BOT_TOKEN: 'test-slack-bot-token'
 };
